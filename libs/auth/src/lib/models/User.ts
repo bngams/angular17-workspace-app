fdf68f604ec408563b32c9cfddff59e4f060fbf7
root <n.boris@devidia.net>
2024-02-01 13:42:26 +0000

    Lib auth

---
+++ libs/auth/src/lib/models/User.ts
@@ -0,0 +1,4 @@
+export type User = {
+    email: string;
+    pwd: string;
+}
---
+++ libs/auth/src/public-api.ts
@@ -0,0 +1,8 @@
+/*
+ * Public API Surface of auth
+ */
+
+export * from './lib/services/auth.service';
+export * from './lib/pages/auth/auth.component';
+
+export * from './lib/models/User';
